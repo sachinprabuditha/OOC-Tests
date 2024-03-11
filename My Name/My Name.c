7670c77b8eb79f95c147109ac6adc999ff5eadc4
Sachin Prabuditha <125368889+sachinprabuditha@users.noreply.github.com>
2024-03-11 08:10:30 +0530

    Add files via upload

---
+++ My Name/My Name.c
@@ -0,0 +1,13 @@
+#include<studio.h>
+int main(){
+char first_name[5];
+char last_name[10];
+
+    printf("enter the first name\n");
+    scanf("%s",&first_name);
+    printf("enter the last name\n");
+    scanf("%s",&last_name);
+    printf("My Name is %s %s",first_name,last_name);
+
+return 0;
+}
---
+++ My Name/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+char first_name[5];
+char last_name[10];
+
+    printf("enter the first name\n");
+    sacnf("%s",&first_name);
+    printf("enter the last name\n");
+    scanf("%s",&last_name);
+    printf("My Name is %s %s",first_name,last_name);
+
+    return 0;
+}
